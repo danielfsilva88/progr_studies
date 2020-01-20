f7daf354df67d563bd90d3c00acfa4baf877745b
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2020-01-20 09:40:06 -0300

    avancos no ex1 do ultimo teste do advanced R

--- R_estudos/Curso_advanced_R_programming/1_factorial_code.R
+++ R_estudos/Curso_advanced_R_programming/1_factorial_code.R
@@ -1,37 +1,61 @@
-
-
+#### Functions ####
 
 #Factorial_loop: a version that computes the factorial of an integer using looping (such as a for loop)
-Factorial_loop <- function(n){
-    if(n == 0){
-        return 1
+factorial_loop <- function(n){
+  stopifnot(n%%1==0)
+  aux = 1
+  if(n < 0){
+    stop("Doesn't exist negative factorial.")
+  }
+  else if (n > 0){
+    for(i in 1:n){
+      aux <- aux*i
     }
-    else if(n < 0){
-        stop("Doesn't exist negative factorial.")
-    }
-    else{
-        aux = 1
-        for(i in 1:n){
-            
-        }
-    }
+  }
+  return(aux) 
 }
 
-
-#Factorial_reduce: a version that computes the factorial using the reduce() function in the purrr package. Alternatively, you can use the Reduce() function in the base package.
-Factorial_reduce <- function(){
-
+#Factorial_reduce: a version that computes the factorial using the reduce()
+# function in the purrr package. Alternatively, you can use the 
+#Reduce() function in the base package.
+library("purrr")
+factorial_reduce <- function(n){
+  stopifnot(n>=0 & n%%1==0)
+  if(n==0){return(1)}
+  reduce(1:n, function(x,y){x*y})
 }
-
 
 #Factorial_func: a version that uses recursion to compute the factorial.
-Factorial_func <- function(){
-
+factorial_func <- function(n){
+  stopifnot(n>=0 & n%%1==0)
+  if(n==0){
+    return(1)
+  } else{
+      n*factorial_func(n-1)
+    }
 }
-
 
 #Factorial_mem: a version that uses memoization to compute the factorial.
-Factorial_mem <- function(){
-    
+fac_mem <- c(1)
+factorial_mem <- function(n){
+  stopifnot(n>=0 & n%%1==0)
+  if (n==0) { 
+    return(fac_mem[1])
+  } else if(!is.na(fac_mem[n])){
+    return(fac_mem[n])
+  } else{
+    fac_mem[n] <<- n*factorial_mem(n-1)
+    return(fac_mem[n])
+  }
 }
 
+#### Benchmark ####
+
+library(microbenchmark)
+loop_data <- map(1:10, function(x){microbenchmark(factorial_loop(x), times = 100)$time})
+
+reduce_data <- map(1:10, function(x){microbenchmark(factorial_reduce(x), times = 100)$time})
+
+factorial_data <- map(1:10, function(x){microbenchmark(factorial_func(x), times = 100)$time})
+
+mem_data <- map(1:10, function(x){microbenchmark(factorial_mem(x), times = 100)$time})
--- R_estudos/Curso_advanced_R_programming/2_oop_code.R
+++ R_estudos/Curso_advanced_R_programming/2_oop_code.R
@@ -1,10 +1,10 @@
-#You will need to design a class called “LongitudinalData” that
+#You will need to design a class called "LongitudinalData" that
 # characterizes the structure of this longitudinal dataset. You will
-# also need to design classes to represent the concept of a “subject”, a “visit”, and a “room”.
+# also need to design classes to represent the concept of a subject, a visit, and a room.
 
 #In addition you will need to implement the following functions
 
-#make_LD: a function that converts a data frame into a “LongitudinalData” object
+#make_LD: a function that converts a data frame into a LongitudinalData object
 make_LD <- function(){
 
 }
