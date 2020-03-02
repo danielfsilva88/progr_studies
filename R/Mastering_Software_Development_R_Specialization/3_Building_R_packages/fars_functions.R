cf65787d765ccef074dc7d53298487abedf7340e
Daniel Ferreira Silva <daniel.ferreira.silva88@gmail.com>
2020-03-01 21:32:06 -0300

    restructure folder & progress in fars ex

--- R/Mastering_Software_Development_R_Specialization/3_Building_R_packages/fars_functions.R
+++ R/Mastering_Software_Development_R_Specialization/3_Building_R_packages/fars_functions.R
@@ -1,8 +1,4 @@
-#' "fars"
-#' 
-#' This script have functions to read and manipulate fars data.
-#' 
-#' Read file with fars data
+#' Read fars data file
 #' 
 #' This function checks if a string passed as argument is a existing csv file, 
 #' extract it and returns a \code{tbl_df} data
@@ -11,10 +7,12 @@
 #' 
 #' @return This function returns a data structure of type \code{tbl_df}
 #' 
-#' @examples
-#' fars_read("file.csv")
+#' @importFrom readr read_csv
+#' @importFrom dplyr tbl_df
 #' 
-#' @export
+#' @examples
+#' \dontrun{fars_read("file.csv")}
+#' 
 fars_read <- function(filename) {
         if(!file.exists(filename))
                 stop("file '", filename, "' does not exist")
@@ -23,7 +21,6 @@ fars_read <- function(filename) {
         })
         dplyr::tbl_df(data)
 }
-
 
 #' Generates a filename with a specified year
 #' 
@@ -35,28 +32,30 @@ fars_read <- function(filename) {
 #' @return A filename string with input received 
 #' 
 #' @examples
-#' make_filename(2018)
-#' make_filename("2018")
-#' make_filename(c("2018", "2019"))
+#' \dontrun{make_filename(2018)}
+#' \dontrun{make_filename(c("2018", "2019"))}
 #' 
-#' @export
 make_filename <- function(year) {
         year <- as.integer(year)
         sprintf("accident_%d.csv.bz2", year)
 }
 
-#' Read file with fars data
+#' Read files with fars data
 #' 
-#' This function checks if a string passed as argument is a existing csv file, 
-#' extract it and returns a \code{tbl_df} data
+#' This function gets an array of string or int with years
+#' and returns a list of fars data corresponding to years inputed or
+#' a warning that there is not a fars data to a specific year
 #' 
 #' @param years String containing the name of the csv file to be read
 #' 
-#' @return This function returns a data structure of type \code{tbl_df}
+#' @return This function returns a list of \code{tbl_df}
+#' 
+#' @importFrom dplyr mutate, "%>%", select
 #' 
 #' @examples
-#' fars_read_years()
-#' fars_read_years()
+#' \dontrun{fars_read_years(c(2013, 2014, 2015))}
+#' \dontrun{fars_read_years("2014")}
+#' \dontrun{fars_read_years(1910)}
 #' 
 #' @export
 fars_read_years <- function(years) {
@@ -73,39 +72,46 @@ fars_read_years <- function(years) {
         })
 }
 
-#' Read file with fars data
+#' Summarize fars data
 #' 
-#' This function checks if a string passed as argument is a existing csv file, 
-#' extract it and returns a \code{tbl_df} data
+#' This function gets an array of string or int with years
+#' and returns how many fars data it happened by month of a year
 #' 
 #' @param years String containing the name of the csv file to be read
 #' 
 #' @return This function returns a data structure of type \code{tbl_df}
 #' 
+#' @importFrom dplyr bind_rows, group_by, summarize, n
+#' @importFrom tidyr spread
+#' 
 #' @examples
-#' fars_summarize_years(years)
+#' \dontrun{fars_summarize_years(c(2014, 2015))}
 #' 
 #' @export
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>% 
                 dplyr::group_by(year, MONTH) %>% 
-                dplyr::summarize(n = n()) %>%
+                dplyr::summarize(n = dplyr::n()) %>%
                 tidyr::spread(year, n)
 }
 
-#' Read file with fars data
+#' Print fars data in a plot
 #' 
-#' This function checks if a string passed as argument is a existing csv file, 
-#' extract it and returns a \code{tbl_df} data
+#' This function gets two strings representing state number and a year
+#' check if state number is valid and plot fars data in map of state
 #' 
-#' @param state.num String containing the name of the csv file to be read
-#' @param year String containing the name of the csv file to be read
+#' @param state.num String containing a number of USA state
+#' @param year String of a year
 #' 
-#' @return This function returns a data structure of type \code{tbl_df}
+#' @return This function plots fars data in specified state
+#' 
+#' @importFrom dplyr filter
+#' @importFrom maps map
+#' @importFrom graphics points
 #' 
 #' @examples
-#' fars_map_state(state.num, year)
+#' \dontrun{fars_map_state(52, 2014)}
 #' 
 #' @export
 fars_map_state <- function(state.num, year) {
