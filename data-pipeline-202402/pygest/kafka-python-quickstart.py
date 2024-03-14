4706da2e79806d6d712a3b02508449d89b8cf7d5
daniel <daniel.ferreira.silva88@gmail.com>
2024-03-13 21:48:04 -0300

    refactor kafka quickstart to send json msgs

--- data-pipeline-202402/pygest/kafka-python-quickstart.py
+++ data-pipeline-202402/pygest/kafka-python-quickstart.py
@@ -1,20 +1,24 @@
 from kafka import KafkaProducer, KafkaConsumer
-import time
+import time, json
 
 # Kafka broker(s) address
 bootstrap_servers = 'localhost:9092'
 
 # Create a Kafka producer
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+#producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                         value_serializer=lambda m: json.dumps(m).encode('utf-8')
+                         )
 
 # Produce messages to a topic
 topic = 'quickstart-topic'
 
-for i in range(10):
-    message = f'Message {i}'
-    producer.send(topic, message.encode('utf-8'))
-    print(f"Produced: {message}")
-    time.sleep(1)
+for i in range(2):
+  #message = f'Message {i}'
+  #producer.send(topic, message.encode('utf-8'))
+  message = {"msg_id": i, "msg": f"Message {i}", "msg_timestamp": time.strftime("%Y-%m-%d %H:%M:%S")}
+  producer.send(topic, message)
+  print(f"Produced: {message}")
 
 # Flush the producer to ensure all messages are sent
 producer.flush()
@@ -24,7 +28,11 @@ consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_servers, auto_offset
 
 # Consume messages from the topic
 for message in consumer:
+  if message.value:
     print(f"Consumed: {message.value.decode('utf-8')}")
+    break
+  else:
+    print("No new messages available.")
 
 # Close the consumer and producer
 consumer.close()
