d7ecf90102d60e879c9e2c2b1415569d48aad9d2
Daniel <daniel.ferreira.silva88@gmail.com>
2024-03-26 23:58:13 -0300

    flourished postgre_tbl.py

--- data-pipeline-202402/pygest/pygest_v2/postgre_tbl.py
+++ data-pipeline-202402/pygest/pygest_v2/postgre_tbl.py
@@ -1,13 +1,30 @@
 import psycopg2
 
-# Database connection details
-host = "localhost"  
-database = "nome_do_db"
-user = "seu_user"
-password = "sua_senha"
+def postgre_ddl_queries(query, root_stmt):
+  
+  # Database connection details
+  host = "localhost"  
+  database = "nome_do_db"
+  user = "seu_user"
+  password = "sua_senha"
 
-# table creation
-sql_tbl_create = """
+  try:
+    conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+    cur = conn.cursor()
+    cur.execute(query)
+    conn.commit()
+    return_stmt = f"Table {root_stmt}ed successfully!"
+  except (Exception, psycopg2.Error) as error:
+    return_stmt = f"Error while {root_stmt}ing table: {error}"
+  finally:
+    if conn:
+      conn.close()
+  
+  return return_stmt
+
+
+queries_dict = {"drop": "DROP TABLE IF EXISTS pygest_v2;",
+                "creat": """
 CREATE TABLE IF NOT EXISTS pygest_v2 (
   id SERIAL PRIMARY KEY,
   kafka_data JSONB NOT NULL,
@@ -20,17 +37,10 @@ comment on column pygest_v2.kafka_producer_timestamp is 'Time when data got into
 comment on column pygest_v2.kafka_consumer_timestamp is 'Time when data was read by pygest consumer';
 comment on column pygest_v2.ingestion_timestamp is 'Time UTC-3 when data was ingested by DB';
 """
+}
 
-try:
-  conn = psycopg2.connect(host=host, database=database, user=user, password=password)
-  cur = conn.cursor()
-  cur.execute(sql_tbl_create)
-  conn.commit()
-  print("Table created successfully!")
-except (Exception, psycopg2.Error) as error:
-  print("Error while creating table:", error)
-finally:
-  if conn:
-    conn.close()
+for k, v in queries_dict.items():
+  return_stmt = postgre_ddl_queries(v, k)
+  print(return_stmt)
 
-print("End of quickstart.")
+print("End of postgre config.")
