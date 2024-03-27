11052fe08cb8f45f29b4daa749f3daa5e574fc54
Daniel <daniel.ferreira.silva88@gmail.com>
2024-03-26 22:53:23 -0300

    adapted DDL to timestamp with time zone

--- data-pipeline-202402/pygest/pygest_v2/postgre_tbl.py
+++ data-pipeline-202402/pygest/pygest_v2/postgre_tbl.py
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS pygest_v2 (
   kafka_data JSONB NOT NULL,
   kafka_producer_timestamp TIMESTAMP NOT NULL,
   kafka_consumer_timestamp TIMESTAMP NOT NULL,
-  ingestion_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP - interval '3 hour'
+  ingestion_timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
 comment on column pygest_v2.kafka_producer_timestamp is 'Time when data got into Kafka topic';
